07c2c5283db87b4de9cde41176f585abf200efd1
Tass2T <brunog90@gmail.com>
2023-11-26 22:55:42 +0100

    added redirection to login if no session

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,11 +1,21 @@
 import Topbar from '@/components/Topbar';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 interface Homeprops {
     chidren: React.ReactNode;
 }
 
-export default function Home({ chidren }: Homeprops) {
+export default async function Home({ chidren }: Homeprops) {
     const images = [];
+
+    const supabase = createServerComponentClient({ cookies });
+    const {
+        data: { session },
+    } = await supabase.auth.getSession();
+
+    if (!session) redirect('login');
 
     return (
         <div className="w-full h-full">
