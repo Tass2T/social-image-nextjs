85449b6616a794c10a2bf6502803fe05090ddf1d
Tass2T <brunog90@gmail.com>
2023-11-27 02:28:22 +0100

    main page => cleaned up

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,17 +1,5 @@
-import Topbar from '@/components/Topbar';
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
-
-export default async function Home() {
+export default function Home() {
     const images = [];
-
-    const supabase = createServerComponentClient({ cookies });
-    const {
-        data: { session },
-    } = await supabase.auth.getSession();
-
-    if (!session) redirect('login');
 
     return <div className="w-full h-full">main page</div>;
 }
