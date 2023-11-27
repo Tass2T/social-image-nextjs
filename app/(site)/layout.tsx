7973a05cd5513ac9bab6065ec3ff1bc594f348ae
tass2t <brunog90@gmail.com>
2023-11-27 15:47:16 +0100

    redirect if not logged

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,10 +1,20 @@
 import Topbar from '@/components/Topbar';
+import { createServerActionClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 interface SiteLayoutProps {
     children: React.ReactNode;
 }
 
-const SiteLayout = ({ children }: SiteLayoutProps) => {
+const SiteLayout = async ({ children }: SiteLayoutProps) => {
+    const supabase = createServerActionClient({ cookies });
+
+    const {
+        data: { session },
+    } = await supabase.auth.getSession();
+    if (!session) redirect('/login');
+
     return (
         <>
             <Topbar />
